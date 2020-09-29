07c767573ecdb41d3736944d479ebe9bbfb3e98c
TienNguyen <ta7.ddinhftieen@gmail.com>
2020-09-29 18:00:12 +0700

    update

--- app/src/main/java/com/example/wtwatch/MainActivity.java
+++ app/src/main/java/com/example/wtwatch/MainActivity.java
@@ -17,14 +17,15 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager senSensorManager;
-    private Sensor senAccelerometer;
+    private Sensor senAccelerometer, senGyroscope;
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
     private static final int SHAKE_THRESHOLD = 600;
     private boolean gotoTwoPhase = true;
-    private double numberAcceleration = 0;
-    private double minSAcceleration = 19.6;
-    private double maxSAcceleration = -19.6;
+    private int numberAcceleration = 0;
+    private int numberGyrocope= 0;
+    private double minSAcceleration = 9.8;
+    private double maxSAcceleration = -9.8;
     private double smvFF = 7.5;
     private double smvIP = 18.5;
     TextView fallText;
@@ -36,7 +37,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senGyroscope = senSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senGyroscope , SensorManager.SENSOR_DELAY_NORMAL);
         fallText = findViewById(R.id.fall);
     }
 
@@ -44,30 +47,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            Log.i("x", "" + x);
-            Log.i("y", "" + y);
-            Log.i("x", "" + z);
-            double smv = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)
-                    + Math.pow(z, 2));
-            if(gotoTwoPhase){
-                numberAcceleration++;
-                if(minSAcceleration > smv) minSAcceleration = smv;
-                if(maxSAcceleration < smv) maxSAcceleration = smv;
-                if(numberAcceleration == 10){
-                    gotoTwoPhase = false;
-                    if(Math.abs(maxSAcceleration - minSAcceleration) > smvIP){
-                        fallText.setText("Fall detection");
-                    }
-                }
-            }else{
-                if(smv > smvFF){
-                    gotoTwoPhase = true;
-                }
-            }
+//            float x = event.values[0];
+//            float y = event.values[1];
+//            float z = event.values[2];
+            numberAcceleration += 1;
+//            double smv = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)
+//                    + Math.pow(z, 2));
+//            if(gotoTwoPhase){
+//                numberAcceleration += 1;
+//                if(minSAcceleration > smv) minSAcceleration = smv;
+//                if(maxSAcceleration < smv) maxSAcceleration = smv;
+//                if(numberAcceleration == 10){
+//                    gotoTwoPhase = false;
+//                    if(Math.abs(maxSAcceleration - minSAcceleration) > smvIP){
+//                        fallText.setText("Fall detection");
+//                    }
+//                }
+//            }else{
+//                if(smv > smvFF){
+//                    gotoTwoPhase = true;
+//                }
+//            }
         }
+        if(mySensor.getType() == Sensor.TYPE_GYROSCOPE){
+            numberGyrocope ++;
+        }
+        Log.i("acceleration", ""+ numberAcceleration);
+        Log.i("gryocope", ""+ numberGyrocope);
     }
 
     @Override
@@ -82,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     protected void onResume() {
         super.onResume();
+        senSensorManager.registerListener(this, senGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
